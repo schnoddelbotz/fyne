5403ecafe87f3d3bfa9673b5a640ef6d5524ee49
Tilo Prütz <tilo@pruetz.net>
2026-06-08 07:32:22 +0200

    [internal/color] move test into own package

--- internal/color/color_test.go
+++ internal/color/color_test.go
@@ -1,23 +1,25 @@
-package color
+package color_test
 
 import (
-	"image/color"
+	imagecolor "image/color"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
+
+	"fyne.io/fyne/v2/internal/color"
 )
 
-func Test_UnmultiplyAlpha(t *testing.T) {
-	c := color.RGBA{R: 100, G: 100, B: 100, A: 100}
-	r, g, b, a := unmultiplyAlpha(c)
+func Test_ToNRGBA_unmultiplyAlpha(t *testing.T) {
+	c := imagecolor.RGBA{R: 100, G: 100, B: 100, A: 100}
+	r, g, b, a := color.ToNRGBA(c)
 
 	assert.Equal(t, 255, r)
 	assert.Equal(t, 255, g)
 	assert.Equal(t, 255, b)
 	assert.Equal(t, 100, a)
 
-	c = color.RGBA{R: 100, G: 100, B: 100, A: 255}
-	r, g, b, a = unmultiplyAlpha(c)
+	c = imagecolor.RGBA{R: 100, G: 100, B: 100, A: 255}
+	r, g, b, a = color.ToNRGBA(c)
 
 	assert.Equal(t, 100, r)
 	assert.Equal(t, 100, g)
