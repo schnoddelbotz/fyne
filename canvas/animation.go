e37b0e74b13330c0da68437d855e8ccf64300550
Tilo Prütz <tilo@pruetz.net>
2026-06-09 09:10:37 +0200

    [canvas] avoid integer overflow conversion by tighten types

--- canvas/animation.go
+++ canvas/animation.go
@@ -32,21 +32,23 @@ func NewColorRGBAAnimation(start, stop color.Color, d time.Duration, fn func(col
 	r1, g1, b1, a1 := start.RGBA()
 	r2, g2, b2, a2 := stop.RGBA()
 
-	rStart := int(r1 >> 8)
-	gStart := int(g1 >> 8)
-	bStart := int(b1 >> 8)
-	aStart := int(a1 >> 8)
-	rDelta := float32(int(r2>>8) - rStart)
-	gDelta := float32(int(g2>>8) - gStart)
-	bDelta := float32(int(b2>>8) - bStart)
-	aDelta := float32(int(a2>>8) - aStart)
+	rStart := uint8(r1 >> 8)
+	gStart := uint8(g1 >> 8)
+	bStart := uint8(b1 >> 8)
+	aStart := uint8(a1 >> 8)
+	rDelta := int(r2>>8) - int(rStart)
+	gDelta := int(g2>>8) - int(gStart)
+	bDelta := int(b2>>8) - int(bStart)
+	aDelta := int(a2>>8) - int(aStart)
 
 	return &fyne.Animation{
 		Duration: d,
 		Tick: func(done float32) {
 			fn(color.RGBA{
-				R: scaleChannel(rStart, rDelta, done), G: scaleChannel(gStart, gDelta, done),
-				B: scaleChannel(bStart, bDelta, done), A: scaleChannel(aStart, aDelta, done),
+				R: scaleChannel(rStart, rDelta, done),
+				G: scaleChannel(gStart, gDelta, done),
+				B: scaleChannel(bStart, bDelta, done),
+				A: scaleChannel(aStart, aDelta, done),
 			})
 		},
 	}
@@ -124,8 +126,8 @@ func advanceShaderTime(elapsed time.Duration, lastTick, now time.Time) (time.Dur
 	return elapsed, now
 }
 
-func scaleChannel(start int, diff, done float32) uint8 {
-	return uint8(start + int(diff*done))
+func scaleChannel(start uint8, diff int, done float32) uint8 {
+	return start + uint8(float32(diff)*done)
 }
 
 func scaleVal(start float32, delta, done float32) float32 {
