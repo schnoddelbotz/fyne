89eff925b13013ac9f86e575cc4ee6a4bb0d8730
Andy Williams <andy@andy.xyz>
2026-06-27 19:35:06 +0100

    Use updated dep that fixes the native hooks

--- internal/driver/glfw/window_x11.go
+++ internal/driver/glfw/window_x11.go
@@ -1,12 +1,10 @@
-//go:build !wayland && (linux || freebsd || openbsd || netbsd) && !wasm && !test_web_driver
+//go:build x11 && !wayland && (linux || freebsd || openbsd || netbsd) && !wasm && !test_web_driver
 
 package glfw
 
 import "C"
 
 import (
-	"fmt"
-
 	"fyne.io/fyne/v2/driver"
 )
 
@@ -16,17 +14,6 @@ var _ driver.NativeWindow = (*window)(nil)
 func (w *window) RunNative(f func(any)) {
 	context := driver.X11WindowContext{}
 	if v := w.view(); v != nil {
-		// Compiled for both, runtime check - run = X11, panic = Weston
-		// TODO fix if this is GLFW v3.4 auto-detect, Wayland running but X code built
-		// https://github.com/go-gl/glfw/issues/419
-		defer func() {
-			if r := recover(); r != nil {
-				fmt.Println("Recovered X11 / Wayland GLFW conflict", r)
-
-				f(driver.WaylandWindowContext{})
-			}
-		}() // will panic and return without falling through
-
 		context.WindowHandle = uintptr(v.GetX11Window())
 	}
 
